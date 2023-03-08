0bf8dda13b0cc08240a0499c02c619cd062aff7e
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-03-09 01:37:52 +0200

    currecies from api

--- frontend/src/scripts/pages/exchangePage.js
+++ frontend/src/scripts/pages/exchangePage.js
@@ -1,5 +1,24 @@
 import getPageTemplate from '../utils/getPageTemplate';
+import api from '../components/api';
+
+function getCurrencyElement({ code, amount }) {
+  return `<li class="item-row">
+  <span class="currency-name">${code}</span>
+  <span class="dots"></span>
+  <span class="price">${amount}</span>
+</li>
+  `;
+}
 
 export default function exchangePage() {
-  return getPageTemplate('exchange-page');
+  const page = getPageTemplate('exchange-page');
+  const userCurrenciesList = page.querySelector('.users-currencies-list');
+
+  api.getCurrencies().then((data) => {
+    userCurrenciesList.innerHTML = Object.entries(data)
+      .map(([code, { amount }]) => getCurrencyElement({ code, amount }))
+      .join('');
+  });
+
+  return page;
 }
